59b40ae4d78266dffcb8d32f610a1f63ab4b9a74
Atte Kivistö <atte.t.kivisto@gmail.com>
2023-06-05 14:04:26 +0300

    linting added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  console.log('where is this???')
   return (
     <View style={styles.container}>
       <Text>Open up App.js. WADAP??</Text>
